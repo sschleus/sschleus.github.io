bab57c455efeffc22d5b967e231c31e93fb8d120
Shan SCHLEUSNER <73426803+sschleus@users.noreply.github.com>
2024-10-31 07:59:17 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,16 @@
-/* eslint-disable no-undef, no-unused-vars */
 
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
-var points = [];
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  // Put setup code here
-  fill("black");
-  textSize(40);
-  button = createButton("Clear");
-  button.position(30, 85);
-  button.mousePressed(resetpoints);
-}
-
-function resetpoints() {
-  points = [];
-}
-
-function draw() {
-  // Put drawings here
-  background(200);
-  for (i in points) {
-    ellipse(points[i].x, points[i].y, 4, 4);
-  }
-  text("Hello world", 30, 50);
-}
-
-function mousePressed() {
-  points.push(new Point(mouseX, mouseY));
-}
-
-// This Redraws the Canvas when resized
-windowResized = function () {
-  resizeCanvas(windowWidth, windowHeight);
+const sketch_input = (sketch) => {
+    sketch.setup = () => setup_input(sketch);
+    sketch.draw = () => draw_input(sketch);
+    sketch.mousePressed = () => add_points(sketch);
 };
+
+
+const sketch_zone2 = (sketch) => {
+    sketch.setup = () => setup_zone2(sketch); 
+    sketch.draw = () => draw_zone2(sketch); 
+};
+
+
+new p5(sketch_input);
+new p5(sketch_zone2);
